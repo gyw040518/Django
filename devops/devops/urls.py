fd261082e49c8cc9edf943d35ab5834641dcd5e4
ywguo <ywguo@lewatek.com>
2016-03-02 23:09:18 +0800

    home app add a templates

--- devops/devops/urls.py
+++ devops/devops/urls.py
@@ -5,6 +5,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'devops.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', 'home.views.home', name='home'),
     url(r'^admin/', include(admin.site.urls)),
 )
